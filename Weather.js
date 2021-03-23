3be7ed5ee54fbc247577c092b7d12fd1731c2801
Val-code-1 <reit.ben@gmail.com>
2021-03-23 04:17:59 -0500

    Fixed time correctly

--- Weather.js
+++ Weather.js
@@ -30,23 +30,23 @@
 // Time
 const body = document.getElementById("body");
 function timeConverter(UNIX_timestamp) {
-  var a = new Date(UNIX_timestamp * 1000);
+  var a = new Date((UNIX_timestamp - 18000) * 1000);
   var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
-  var hour = a.getHours() - 5;
+  var hour = a.getHours();
   var min = a.getMinutes().toFixed(0);
   var time = date + " " + month + " " + year + " " + hour + ":" + min + "0";
   return time;
 }
 function dayConvert(UNIX_timestamp) {
-  var a = new Date(UNIX_timestamp * 1000);
+  var a = new Date((UNIX_timestamp - 18000) * 1000);
   var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
-  var hour = a.getHours() - 5;
+  var hour = a.getHours();
   var min = a.getMinutes().toFixed(0);
   var time = date + " " + month + " " + year + " " + hour + ":" + min + "0";
   return date;
